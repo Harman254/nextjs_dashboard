5ad1ac8e75f32f9756230f31602df80b96dba447
Harman <harmanmalova@gmail.com>
2023-10-28 12:57:25 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,12 @@
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({ weight: ["400", "700"] , subsets: ["latin"]});
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
