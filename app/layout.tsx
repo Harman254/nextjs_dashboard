4809a9736db08b8b8da403861f8301260e0bcf2c
Harman <harmanmalova@gmail.com>
2023-10-28 13:05:24 +0300

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
+import { inter } from './ui/fonts';
 
-export const lusitana = Lusitana({ weight: ["400", "700"] , subsets: ["latin"]});
-
-
-export const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
